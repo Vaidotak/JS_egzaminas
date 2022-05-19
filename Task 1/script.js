9dc16eb9f5c199f57e424791137eef612187d07b
Vaidotak <pehtrauhk@gmail.com>
2022-05-19 10:43:05 +0300

    Parašytas 2 užduoties kodas

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,29 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document.getElementById("output").style.visibility = "hidden";
+
+const inputValue = document.getElementById("search");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("output").style.visibility = "visible";
+  
+  const kiloG = inputValue.value;
+  const output = document.getElementById("output");
+  const lb = document.createElement("div");
+  lb.innerHTML = Math.floor(kiloG) * 2.2046 + ` pounds`;
+  output.appendChild(lb);
+});
+
+//     document.getElementById("output").style.visibility = "visible";
+  
+  
+// console.log(output)
+//   document.getElementById("lbOutput").innerHTML = kg * 2.2046;
+
+//   document.getElementById("gOutput").innerHTML = kg / 0.0010000;
+
+//   document.getElementById("ozOutput").innerHTML = kg * 35.274;
+
--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,9 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+let clicks = 0;
+
+document.getElementById("btn__element").addEventListener("click", function () {
+  clicks += 1;
+  document.getElementById("btn__state").innerHTML = clicks;
+});
