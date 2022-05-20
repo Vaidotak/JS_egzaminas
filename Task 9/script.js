56c3cb958fae220aba0de93ceb5e13e628865cbd
Vaidotak <pehtrauhk@gmail.com>
2022-05-20 16:33:07 +0300

    9 užduotis padaryta

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,29 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    if (this.budget >= 100000000) {
+      return console.log(
+        "Serialo biudžetas didesnis už leistiną 100000000 USD",
+        true
+      );
+    } else {
+      return console.log(
+        "Serialo biudžetas mažesnis už leistiną 100000000 USD",
+        false
+      );
+    }
+  }
+}
+
+const series = new Movie("Lost", "J.J. Abrams", 1400000000);
+series.wasExpensive();
+console.log("Filmo biudžetas: ", series.budget, "USD");
+
