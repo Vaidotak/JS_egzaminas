5b659f2c289545fbbe3268cd6226c93804058334
Vaidotak <pehtrauhk@gmail.com>
2022-05-20 15:53:31 +0300

    8 užduotis padaryta

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator(first, second) {
+  this.first = first;
+  this.second = second;
+
+  this.sum = () => {
+    return this.first + this.second;
+  };
+  this.subtraction = () => {
+    return this.first - this.second;
+  };
+  this.multiplication = () => {
+    return this.first * this.second;
+  };
+  this.division = () => {
+    return this.first / this.second;
+  };
+}
+
+const newCalc = new Calculator(10, 4);
+
+console.log("Sum: ", newCalc.sum());
+console.log("Subtraction: ", newCalc.subtraction());
+console.log("Multiplication: ", newCalc.multiplication());
+console.log("Division: ", newCalc.division());
+
